f4e7f828e58d0538a9c03c16da7fd5ade66d00a6
albert.grislis <albert.grislis@icloud.com>
2022-07-27 15:57:31 +0300

    Fix initializer access level

--- Sources/LayoutKit/LayoutAnchorProperty.swift
+++ Sources/LayoutKit/LayoutAnchorProperty.swift
@@ -11,10 +11,17 @@ public struct LayoutAnchorProperty<Anchor: LayoutAnchor> {
     
     // MARK: Internal properties
     let anchor: Anchor
+    
+    // MARK: initializers & deinitializers
+    public init(
+        anchor: Anchor
+    ) {
+        self.anchor = anchor
+    }
 }
 
 extension LayoutAnchorProperty {
-
+    
     // MARK: Internal methods
     @usableFromInline
     func getEqual(to otherAnchor: Anchor,
@@ -22,33 +29,33 @@ extension LayoutAnchorProperty {
         return self.anchor.constraint(equalTo: otherAnchor,
                                       constant: constant)
     }
-
+    
     func setEqual(to otherAnchor: Anchor,
                   offsetBy constant: CGFloat = 0) {
         self.anchor.constraint(equalTo: otherAnchor,
                                constant: constant).isActive = true
     }
-
+    
     @usableFromInline
     func getGreaterThanOrEqual(to otherAnchor: Anchor,
                                offsetBy constant: CGFloat = 0) -> NSLayoutConstraint {
         return self.anchor.constraint(greaterThanOrEqualTo: otherAnchor,
                                       constant: constant)
     }
-
+    
     func setGreaterThanOrEqual(to otherAnchor: Anchor,
                                offsetBy constant: CGFloat = 0) {
         self.anchor.constraint(greaterThanOrEqualTo: otherAnchor,
                                constant: constant).isActive = true
     }
-
+    
     @usableFromInline
     func getLessThanOrEqual(to otherAnchor: Anchor,
                             offsetBy constant: CGFloat = 0) -> NSLayoutConstraint {
         return self.anchor.constraint(lessThanOrEqualTo: otherAnchor,
                                       constant: constant)
     }
-
+    
     func setLessThanOrEqual(to otherAnchor: Anchor,
                             offsetBy constant: CGFloat = 0) {
         self.anchor.constraint(lessThanOrEqualTo: otherAnchor,
